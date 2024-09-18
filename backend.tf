5237ebf243cdd7b9367cb6c63bd320fb2c27b775
Wanderson Pereira Maria <wandersonp@ciandt.com>
2024-09-17 22:53:10 -0300

    fix/ajuste na informação da instancia do bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-fiap"
+    bucket         = "terraform-state-bucket-unique-fiap"
     key            = "terraform/state"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-fiap"
