33977c3265c89582b9b5f3ab40c1c626cc1e915c
Wanderson Pereira Maria <wandersonp@ciandt.com>
2024-09-18 21:10:17 -0300

    fix/outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "db_instance_endpoint" {
   description = "Endpoint do banco de dados."
-  value       = aws_db_instance.db.db_name
+  value       = aws_db_instance.fiap-self-service-db.db_name
 }
