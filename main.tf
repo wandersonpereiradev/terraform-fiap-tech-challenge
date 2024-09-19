f8817eda70cca584ce3aa15f829b1f1344d03767
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2024-09-19 11:08:48 -0300

    fix/restaurando main.tf para corrigir erro de pan

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "aws_db_instance" "db_instance" {
   engine               = var.engine
   engine_version       = var.engine_version
   instance_class       = var.instance_class
-  db_name              = var.db_name
-  username             = var.db_user
-  password             = var.db_password
+  db_name              = "fiapSelfServiceDb"
+  username             = "root"
+  password             = "root"
   parameter_group_name = var.parameter_group
   publicly_accessible  = false
   skip_final_snapshot  = true
