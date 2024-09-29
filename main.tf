dd8d2bdef4f8f8c2cdbd81a62fd9b17ec9026aea
Wanderson Pereira <wanderson.p.ayres@gmail.com>
2024-09-29 11:39:42 -0300

    fix/subnet document db

--- main.tf
+++ main.tf
@@ -14,41 +14,78 @@ resource "aws_db_instance" "db_instance" {
   publicly_accessible  = false
   skip_final_snapshot  = true
   apply_immediately    = true
-
-  # Configurações de backup e retenção
-  backup_retention_period = 7
-  backup_window           = "03:00-04:00"
   
   # Configuração de segurança
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
 }
 
-# Criar VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+# Virtual Private Cloud - Rede virtual AWS
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+module "vpc" {
 
-  tags = {
-    Name = "main-vpc"
+  source = "terraform-aws-modules/vpc/aws" 
+
+  name = "vpc-databases" 
+  cidr = "10.0.0.0/16" 
+
+  azs             = ["us-east-1a", "us-east-1a"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+   public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
 
-# Subredes em diferentes zonas de disponibilidade (AZs)
-resource "aws_subnet" "subnet_1" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  # Zona de disponibilidade 1
+resource "aws_subnet" "database_subnet_az1" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "food_database_subnet_az_1"
+  }
 }
 
-resource "aws_subnet" "subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"  # Zona de disponibilidade 2
+resource "aws_subnet" "database_subnet_az2" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "food_database_subnet_az_2"
+  }
+}
+
+#Subnet group
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = [
+    aws_subnet.database_subnet_az1.id,
+    aws_subnet.database_subnet_az2.id
+    ]
+
+  tags = {
+    Name = "db_subnet_group"
+  }
 }
 
 # Grupo de sub-redes para o DocumentDB
 resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   name       = "fiap-self-service-pagamentos-subnet-group"
-  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+  subnet_ids = [
+    aws_subnet.database_subnet_az1.id,
+    aws_subnet.database_subnet_az2.id
+    ]
 
   tags = {
     Name = "fiap-self-service-pagamentos-subnet-group"
@@ -58,7 +95,7 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
 resource "aws_docdb_cluster_instance" "docdb_instance" {
   identifier           = "fiap-self-service-document-db-instance"
   cluster_identifier   = aws_docdb_cluster.docdb_cluster.id
-  instance_class       = "db.r5.large"
+  instance_class       = var.instance_class
   apply_immediately    = true
 }
 
@@ -71,6 +108,9 @@ resource "aws_docdb_cluster" "docdb_cluster" {
   engine_version       = "4.0.0"
   apply_immediately    = true
   skip_final_snapshot  = true
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_docdb_subnet_group.docdb_subnet_group.name
 }
 
 # DynamoDB Table
@@ -99,6 +139,7 @@ resource "aws_dynamodb_table" "dynamo_db" {
 resource "aws_security_group" "rds_sg" {
   name        = "rds_security_group"
   description = "Security group para o RDS e DocumentDB"
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 3306 # Porta MySQL (RDS)
